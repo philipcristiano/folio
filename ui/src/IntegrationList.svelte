f3fb3d319587c96b3214f14bd2796e4fae85f3de
Philip Cristiano <git@philipcristiano.com>
2023-03-13 11:15:29 -0400

    integrations - delete: Fix path conflict

--- ui/src/IntegrationList.svelte
+++ ui/src/IntegrationList.svelte
@@ -12,7 +12,7 @@
   let transactions = [];
 
   async function getIntegrationNames() {
-    let response = await fetch("/integrations/add", {
+    let response = await fetch("/integration/add", {
         method: "GET",
     });
     let json = await response.json()
@@ -24,7 +24,7 @@
     };
   }
   async function getIntegrationSetup(Name) {
-    let response = await fetch("/integrations/add/" + Name, {
+    let response = await fetch("/integration/add/" + Name, {
         method: "GET",
     });
     let json = await response.json()
@@ -39,7 +39,7 @@
   }
   async function setupIntegration(integration) {
 
-      let response = await fetch("/integrations/add/" + integration.name, {
+      let response = await fetch("/integration/add/" + integration.name, {
           method: "POST",
           headers: {
               'Content-Type': 'application/json'
